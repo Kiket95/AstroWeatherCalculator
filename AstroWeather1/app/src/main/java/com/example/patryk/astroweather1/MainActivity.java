d8bd29fa9f8ab927a92de63fbb2f4eff0ec41027
Kiket95 <patryk.kiket@gmail.com>
2018-07-02 02:43:54 +0200

    FIXES

--- AstroWeather1/app/src/main/java/com/example/patryk/astroweather1/MainActivity.java
+++ AstroWeather1/app/src/main/java/com/example/patryk/astroweather1/MainActivity.java
@@ -38,8 +38,8 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         settingsButton = findViewById(R.id.settingsButton);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         mViewPager = (ViewPager) findViewById(R.id.container);
